cddfb940ced5ae328377ad1b4cd09c45d363440d
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2020-04-08 22:56:57 -0300

    Enviando para Github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import bla from './Title.module.sass'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <main className={bla.container}>
+        <div className={styles.App}>
+          <header className={styles['App-header']}>
+            <img src={logo} className={styles['App-logo']} alt="logo" />
+            <p className={bla.container__title}>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className={styles['App-link']}
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </main>
   );
 }
 
